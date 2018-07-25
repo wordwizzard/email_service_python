2e52175abfd04d86efba334d76691a28af2aea15
kevinsmith <kevin@sparkatm.co.za>
2018-07-25 19:33:21 +0200

    Updates to the email service.

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
-# email_service_python
+# Python based Auto Email Job (email_service_python)
+
+#Aim:
 
 I decided to create this specific script after various people told me to use legacy code to accomplish the intended.
 
 That however "irked" me a bit so I just wrote my own - easy to understand - emailing script in python. 
 
+# Basics:
+
 The script essentially sends a configurable email to 3 - pre configured email addresses. It also attached 3 .csv files to the email. 
 
-This script uses a json file to import the necessary server details to enable us to actually log in to send emails. See the config.json for more details on that.
+# Json Object (configuration file):
 
-I quickly ran the script in a virtual environment with the below dependencies. All working.
+This script uses a json object, (because json is awesome) to import the necessary server details to enable us to actually log in to send emails. 
 
-Just a note - there are file paths required for the script to actually work.
-These file paths are simply the source folder where the actual code is located and the master folder - where the files we want to attach to the email is located.
+See the config.json for the specifics
+
+Only the json object needs to be edited for the script to work. Remember Python 3.6.
+
+# File paths:
+
+Various predetermined file paths are required for the script to actually work.
+1. Source directory.
+2. Storage directory. (where the files we want to attach to the email is located)
 
 # Dependancies:
 Python 3.6
--- email_script.py
+++ email_script.py
@@ -10,27 +10,26 @@ from email import encoders
 
 class FileManager:
     def __init__(self):
-
-        self.filePathMaster = None
+        self.filePathStorage = None
         self.filePathSrc = None
-
         self.host = None
         self.port = None
         self.host_usr = None
         self.host_psw = None
-
         self.adr1 = None
         self.adr2 = None
         self.adr3 = None
-
+        self.message = None
+        self.subject = None
+        self.fileName1 = None
+        self.fileName2 = None
+        self.fileName3 = None
         self.current_date = None
 
     def loadconfig(self):
 
         self.current_date = datetime.now().date()
-
-        self.filePathSrc = os.path.abspath('YOUR SOURCE FILEPATH - WHERE CODE IS LOCATED')
-        self.filePathMaster = os.path.abspath('YOUR MASTER FOLDER - WHERE CSV FILES ARE LOCATED')
+        # TODO:
 
         # Change directory to where the config.json file is located.
         os.chdir(self.filePathSrc)
@@ -39,39 +38,43 @@ class FileManager:
         with open('config.json') as json_data_file:
             data = json.load(json_data_file)
 
-        eS = data["emailServer"]
-        self.host = eS["host"]
-        self.port = eS["port"]
-        self.host_usr = eS["host_user"]
-        self.host_psw = eS["host_password"]
+        server = data["emailServer"]
+        details = data["emailDetails"]
+        paths = data["filePaths"]
+        self.host = server["host"]
+        self.port = server["port"]
+        self.host_usr = server["host_user"]
+        self.host_psw = server["host_password"]
 
-        self.adr1 = eS["address1"]
-        self.adr2 = eS["address2"]
-        self.adr3 = eS["address3"]
+        self.adr1 = details["address1"]
+        self.adr2 = details["address2"]
+        self.adr3 = details["address3"]
 
+        self.message = details["message"]
+        self.subject = details["subject"]
+
+        self.fileName1 = details["filename1"]
+        self.fileName2 = details["filename2"]
+        self.fileName3 = details["filename3"]
+
+        self.filePathSrc = os.path.abspath(paths["source"])
+        self.filePathStorage = os.path.abspath(paths["storage"])
 
     def email_files(self):
         # We should be in the directory where the file we want to attach are located.
-        if os.getcwd() != self.filePathMaster:
-            os.chdir(self.filePathMaster)
+        if os.getcwd() != self.filePathStorage:
+            os.chdir(self.filePathStorage)
 
         try:
-            to_address = self.adr1
-            # These can be added to the config.json file if you want to
-            message = "WRITE SOME KIND OF MESSAGE IN HERE TO INSERT IT IN THE EMAIL"
-            subject = "YOUR SUBJECT HEADING" + " @" + str(self.current_date)    # Added the date here since I needed it.
-
-            file1 = 'FILENAME1.csv'
-            file2 = 'FILENAME2.csv'
-            file3 = 'FILENAME3.csv'
-            files = [file1, file2, file3]
+            # Changeable from the json object
+            to_address = self.adr1 + ',' + self.adr2 + ',' + self.adr3
+            message = self.message
+            subject = self.subject + " @" + str(self.current_date)      # Added the date here since I needed it.
+            files = [self.fileName1, self.fileName2, self.fileName3]
 
             server = smtplib.SMTP(str(self.host) + ":" + str(self.port))
             server.starttls()
             server.login(self.host_usr, self.host_psw)
-
-            print("Receiver address - {to_address}".format(to_address=self.adr1 + ',' + self.adr2 + ',' + self.adr3))
-            print("Building email content")
 
             # Setup the email header details
             msg = MIMEMultipart(
@@ -98,13 +101,12 @@ class FileManager:
         except smtplib.SMTPRecipientsRefused as refused:
             print("Invalid address - {to_address}".format(to_address=self.adr1 + ',' + self.adr2 + ',' + self.adr3))
         finally:
-            print('Done Sending mail')
+            print('Done')
             if server:
                 server.quit()
 
 
 def main():
-    print("Start Program")
 
     fm = FileManager()
     fm.loadconfig()
